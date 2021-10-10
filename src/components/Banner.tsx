c43497a3bf1ac4172127c8426d22a2d069b50771
NearMaick <maick_a_s@msn.com>
2021-10-10 15:04:32 -0300

    style(web): Home Banner #1

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,47 @@
+import { Flex, Heading, Text, Box, Image } from '@chakra-ui/react';
+
 export function Banner(): JSX.Element {
   return (
-    <div>
-      <h1>Banner</h1>
-    </div>
+    <Flex
+      w="100%"
+      h="72"
+      bgImage="url('/assets/banner.jpg')"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <Flex
+        w="100%"
+        justify={{ sm: 'center', lg: 'space-between' }}
+        mx="auto"
+        px={{ base: '4', sm: '10', md: '20', lg: '36' }}
+      >
+        <Flex flexDir="column" justify="center">
+          <Heading
+            color="gray.100"
+            fontWeight="500"
+            fontSize={{ sm: '3xl', lg: '4xl' }}
+          >
+            Cinco continentes, <br />
+            infinitas possibilidades
+          </Heading>
+          <Text
+            color="gray.300"
+            mt="5"
+            fontSize={{ sm: '0.8rem', md: 'xl' }}
+            maxWidth={{ base: '100%', lg: '550px' }}
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou!
+          </Text>
+        </Flex>
+        <Image
+          src="assets/airplane.svg"
+          alt="airplane"
+          w={{ base: '300px', lg: '430px' }}
+          display={{ sm: 'none', lg: 'block' }}
+          transform="translateY(48px)"
+          ml="8"
+        />
+      </Flex>
+    </Flex>
   );
 }
